5ae4e591ad2f27f4985bb1f62a0bf6e3c87d532a
im-ian-m <156471115+im-ian-m@users.noreply.github.com>
2025-04-03 17:46:18 -0400

    change wrapper file

--- lab2/wrapper.vhd
+++ lab2/wrapper.vhd
@@ -4,18 +4,22 @@ USE IEEE.NUMERIC_STD.ALL;
 
 entity wrapper is
     Port (  SW0, SW1, SW2, SW3, SW4, SW5, SW6, SW7 : in std_logic;
-            segment0, segment1, segment2, segment3 : out std_logic(6 downto 0);
             CLK : in std_logic;
+            segment0, segment1, segment2, segment3 : out std_logic_vector(6 downto 0)            
     );
 end wrapper;
 
-signal RB_Out0, RB_Out1, RB_Out2, RB_Out3 : std_logic;
-signal X : unsigned(7 downto 0);
-signal angle : unsigned(9 downto 0);
-signal d0, d1, d2, d3, d4 : std_logic_vector(3 downto 0);
-process (rising_edge(CLK))
+architecture implementation of wrapper is
+    signal RB_Out0, RB_Out1, RB_Out2, RB_Out3 : std_logic;
+    signal Xvec : std_logic_vector(7 downto 0);
+    signal X : unsigned(7 downto 0);
+    signal angle : unsigned(9 downto 0);
+    signal d0, d1, d2, d3, d4 : std_logic_vector(3 downto 0);
 begin
-    X <= unsigned(SW7 & SW6 & SW5 & SW4 & SW3 & SW2 & SW1 & SW0);
+process (CLK)
+begin
+    Xvec <= SW7 & SW6 & SW5 & SW4 & SW3 & SW2 & SW1 & SW0;
+    X <= unsigned(Xvec);
 
     acos : g09_ARCCOS port map (X => X, angle => angle);
     
@@ -27,3 +31,4 @@ begin
     s1 : g09_7_segment_decoder port map(d1 => value, RB_Out2 => RB_In, segments => segment1, RB_Out => RB_Out1);
     s0 : g09_7_segment_decoder port map(d0 => value, RB_Out1 => RB_In, segments => segment0, RB_Out => RB_Out0);
 end process;
+end implementation;
